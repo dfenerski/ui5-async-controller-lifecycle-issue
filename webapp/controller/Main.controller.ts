c875ff8d5bf434a6f71cd2f120c715e52f09ef62
Dimitar Fenerski <dfenerski@gmail.com>
2024-03-25 14:33:30 +0200

    feat: add initial example

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -5,6 +5,10 @@ import BaseController from "./BaseController";
  * @namespace com.github.dfenerski.issue_async_controller_lifecycle.controller
  */
 export default class Main extends BaseController {
+	public async onInit(){
+		MessageBox.show("Hello World!");
+	}
+
 	public sayHello(): void {
 		MessageBox.show("Hello World!");
 	}
